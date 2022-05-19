d58ee354cfb99162e67a591c66b55a292e367250
Asmah Emmanuel <atbhae100@gmail.com>
2022-05-19 09:55:11 -0700

    dbconnectlink

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/gowordsgh")
+  .connect("mongodb+srv://gowordsghana:gowordsghana@cluster0.tsy3z.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err.message));
