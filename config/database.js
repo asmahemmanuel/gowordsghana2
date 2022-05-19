1218645a4e309f0c729ae9df490e20ff7069429a
Asmah Emmanuel <atbhae100@gmail.com>
2022-05-19 07:43:30 -0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/gowordsgh")
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err.message));
